3d22edf130656b808682f41bdc68da06119afece
SlavikGemay <gemayit@gmail.com>
2025-03-13 12:47:42 +0200

    dz 23

---
+++ js/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+document.querySelectorAll('li button').forEach(button => {
+    button.textContent = 'Видалити';
+});
+
+document.querySelector('ul').addEventListener("click", (event) => {
+   if (event.target.nodeName === "BUTTON") {
+       event.target.parentElement.remove();
+   }
+});
+
+document.querySelector('div').addEventListener('click', (event) => {
+    if (event.target.nodeName === "BUTTON") {
+        const li = document.createElement("li");
+        const button = document.createElement('button');
+
+        const addTask = document.querySelector('input');
+        const valueOfTask = addTask.value;
+
+        button.textContent = 'Видалити';
+        li.append(button);
+        li.prepend(valueOfTask);
+
+        document.querySelector("ul").append(li);
+    }
+});
